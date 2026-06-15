ff40fdc11467e2aa40766855e97350cbefcad9bd
J.C. (Jochem, Student B-AT) <j.c.zwier@student.utwente.nl>
2026-06-15 16:10:16 +0200

    Changed the texts in tinyclip

--- TINY_CLIP_TEST.py
+++ TINY_CLIP_TEST.py
@@ -9,7 +9,7 @@ processor = CLIPProcessor.from_pretrained("wkcn/TinyCLIP-ViT-39M-16-Text-19M-YFC
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
 
-inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="pt", padding=True)
+inputs = processor(text=["a photo of a cat", "a photo of a dog","a photo of a couch with cats"], images=image, return_tensors="pt", padding=True)
 
 outputs = model(**inputs)
 logits_per_image = outputs.logits_per_image # this is the image-text similarity score
